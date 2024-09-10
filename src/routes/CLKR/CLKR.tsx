864cb4fdf9065f689aa67f5b03c22e8c06b93f92
NoahK216 <115042520+NoahK216@users.noreply.github.com>
2024-09-10 15:39:11 -0400

    Version bump, as well as upgrading to heroku node24 stack

--- src/routes/CLKR/CLKR.tsx
+++ src/routes/CLKR/CLKR.tsx
@@ -2,12 +2,13 @@
 
 import NavBar from '../../common_components/NavBar';
 import Footer from '../../common_components/Footer';
+import CLKRLanding from './CLKRLanding';
 
 const CLKR = () => {
     return (
         <div className="CLKR">
             <NavBar />
-
+            <CLKRLanding />
             <Footer />
         </div>
     );
---
+++ src/routes/CLKR/CLKRLanding.tsx
@@ -0,0 +1,11 @@
+/* Noah Klein */
+
+const CLKRLanding = ({ }) => {
+    return (
+        <div className="CLKRLanding">
+            <h1>Introducing CLKR</h1>
+        </div>
+    )
+}
+
+export default CLKRLanding;
