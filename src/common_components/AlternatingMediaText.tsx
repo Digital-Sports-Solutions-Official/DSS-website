c8d2b7b1e91ccb79107f0cdc9b49834e211e7823
NoahK216 <115042520+NoahK216@users.noreply.github.com>
2024-09-10 16:28:54 -0400

    Factored alternating media text layout out of partner and used it for Ecosystem as well

---
+++ src/common_components/AlternatingMediaText.tsx
@@ -0,0 +1,52 @@
+/* Noah Klein */
+
+import { CSSProperties } from "react";
+
+export interface MediaTextEntry {
+    title: string;
+    href?: string;
+    imgURL: string;
+    description: string;
+}
+
+interface AlternatingMediaTextProps {
+    entries: Array<MediaTextEntry>;
+}
+
+const AlternatingMediaText = ({ entries }: AlternatingMediaTextProps) => {
+    const isEven = (x: number): boolean => {
+        return (x % 2) === 0;
+    }
+
+    const getTitlePosition = (x: number): CSSProperties => {
+        return isEven(x) ? { position: 'absolute', top: '100%', left: 0 } : { position: 'absolute', top: '100%', right: 0, textAlign: 'end' }
+
+    }
+
+    return (
+        <div className='column'>
+            {Array.from(entries.entries()).map(([index, entry]) => (
+                <div className='row entry' style={{ flexDirection: (isEven(index) ? 'row' : 'row-reverse') }}>
+
+                    <div className='column' style={{ position: 'relative' }}>
+                        {entry.href ? (
+                            <a href={entry.href} target='_blank'>
+                                <img src={entry.imgURL} />
+                            </a>
+                        ) : (
+                            <img src={entry.imgURL} />
+                        )}
+
+                        <div className='title' style={getTitlePosition(index)}>
+                            <h3>{entry.title}</h3>
+                        </div>
+                    </div>
+
+                    <p>{entry.description}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default AlternatingMediaText;
--- src/routes/Home/Partners.tsx
+++ src/routes/Home/Partners.tsx
@@ -1,58 +1,28 @@
 /* Noah Klein */
 
-import { CSSProperties } from 'react';
 import './Partners.css'
+import AlternatingMediaText from '../../common_components/AlternatingMediaText';
 
 interface Partner {
-    name: string;
-    logoUrl: string;
+    title: string;
     href?: string;
+    imgURL: string;
     description: string;
 }
 
 const partnerArray: Array<Partner> = [
-    { name: "NDA", logoUrl: 'NDA-logo.jpg', href: "https://ncdadodgeball.com/", description: "[HEY CHECK OUT MY IMAGE'S HREF!]-----------------------Lorem ipsum odor amet, consectetuer adipiscing elit. Duis nisi netus aptent augue facilisis sociosqu himenaeos turpis. Habitasse convallis ipsum morbi metus massa cursus. Ataciti scelerisque sapien pretium scelerisque erat fermentum congue. Montes curabitur commodo at orci morbi. Posuere adipiscing dui vestibulum; per cras augue arcu?" },
-    { name: "Facebook the country", logoUrl: 'NDA-logo.jpg', description: "[NO HREF SPECIFIED ON THIS ONE, SO NO LINK!]----------------------Mi aliquam ipsum aliquet neque class faucibus ligula. Hac interdum eget tristique blandit nunc aliquet in phasellus. Class ornare nullam congue metus praesent cubilia ridiculus rutrum. Quis porttitor facilisi habitasse adipiscing eu aliquam. Magna enim parturient sit arcu dictum magna feugiat mollis orci? Blandit potenti posuere vehicula; vestibulum habitant penatibus scelerisque. Sollicitudin turpis dictum odio efficitur blandit. Donec inceptos risus sodales sagittis sollicitudin per." },
-    { name: "Maybe this one is long", href: "https://ncdadodgeball.com/", logoUrl: 'NDA-logo.jpg', description: "Fringilla nunc suscipit augue facilisi tellus aliquet ipsum. Dui porta elementum class natoque platea nam curae id malesuada. Vitae auctor venenatis felis a at auctor. Neque sem euismod posuere nullam tempus vitae aenean nostra. Vel suspendisse class nulla vivamus quam consectetur semper. Auctor est aliquet magna ultrices tellus vel lacus ex feugiat. Ad penatibus amet suspendisse dapibus mollis sociosqu. Porttitor a pulvinar penatibus habitasse mi ultricies ex." }
+    { title: "NDA", imgURL: 'NDA-logo.jpg', href: "https://ncdadodgeball.com/", description: "[HEY CHECK OUT MY IMAGE'S HREF!]-----------------------Lorem ipsum odor amet, consectetuer adipiscing elit. Duis nisi netus aptent augue facilisis sociosqu himenaeos turpis. Habitasse convallis ipsum morbi metus massa cursus. Ataciti scelerisque sapien pretium scelerisque erat fermentum congue. Montes curabitur commodo at orci morbi. Posuere adipiscing dui vestibulum; per cras augue arcu?" },
+    { title: "Facebook the country", imgURL: 'NDA-logo.jpg', description: "[NO HREF SPECIFIED ON THIS ONE, SO NO LINK!]----------------------Mi aliquam ipsum aliquet neque class faucibus ligula. Hac interdum eget tristique blandit nunc aliquet in phasellus. Class ornare nullam congue metus praesent cubilia ridiculus rutrum. Quis porttitor facilisi habitasse adipiscing eu aliquam. Magna enim parturient sit arcu dictum magna feugiat mollis orci? Blandit potenti posuere vehicula; vestibulum habitant penatibus scelerisque. Sollicitudin turpis dictum odio efficitur blandit. Donec inceptos risus sodales sagittis sollicitudin per." },
+    { title: "Maybe this one is long", href: "https://ncdadodgeball.com/", imgURL: 'NDA-logo.jpg', description: "Fringilla nunc suscipit augue facilisi tellus aliquet ipsum. Dui porta elementum class natoque platea nam curae id malesuada. Vitae auctor venenatis felis a at auctor. Neque sem euismod posuere nullam tempus vitae aenean nostra. Vel suspendisse class nulla vivamus quam consectetur semper. Auctor est aliquet magna ultrices tellus vel lacus ex feugiat. Ad penatibus amet suspendisse dapibus mollis sociosqu. Porttitor a pulvinar penatibus habitasse mi ultricies ex." }
 ]
 
 const Partners = ({ }) => {
-    const isEven = (x: number): boolean => {
-        return (x % 2) === 0;
-    }
-
-    const getTitlePosition = (x: number): CSSProperties => {
-        return isEven(x) ? { position: 'absolute', top: '100%', left: 0 } : { position: 'absolute', top: '100%', right: 0, textAlign: 'end' }
-
-    }
-
     return (
         <div className="Partners">
             <h1>Partners</h1>
-            <div className='column'>
-                {Array.from(partnerArray.entries()).map(([index, partner]) => (
-                    <div className='row partner' style={{ flexDirection: (isEven(index) ? 'row' : 'row-reverse') }}>
-
-                        <div className='column' style={{ position: 'relative' }}>
-                            {partner.href ? (
-                                <a href={partner.href} target='_blank'>
-                                    <img src={partner.logoUrl} />
-                                </a>
-                            ) : (
-                                <img src={partner.logoUrl} />
-                            )}
-
-                            <div className='title' style={getTitlePosition(index)}>
-                                <h3>{partner.name}</h3>
-                            </div>
-                        </div>
-
-                        <p>{partner.description}</p>
-                    </div>
-                ))}
-
-
-            </div>
+            <AlternatingMediaText
+                entries={partnerArray}
+            />
         </div >
     )
 }
