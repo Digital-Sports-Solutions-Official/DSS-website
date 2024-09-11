f7a8a814006d20f27d9813241252f2aa8ab1fa32
NoahK216 <115042520+NoahK216@users.noreply.github.com>
2024-09-10 22:25:28 -0400

    Made landing mobile-friendly

--- src/routes/Home/Landing.tsx
+++ src/routes/Home/Landing.tsx
@@ -13,7 +13,7 @@ const Landing = ({ }) => {
                     <div>Sports</div>
                     <p>Digital Sports Solutions LLC.</p>
                 </div>
-                <div className="column right">
+                <div className="column image-paragraph">
                     <img src='https://computercare.net/wp-content/uploads/2019/08/Blog-Post-Header.png' />
                     <p>
                         Lorem ipsum odor amet, consectetuer adipiscing elit. Duis nisi netus aptent augue facilisis sociosqu himenaeos turpis. Habitasse convallis ipsum morbi metus massa cursus. Ataciti scelerisque sapien pretium scelerisque erat fermentum congue. Montes curabitur commodo at orci morbi. Posuere adipiscing dui vestibulum; per cras augue arcu?
