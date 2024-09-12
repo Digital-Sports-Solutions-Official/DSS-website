caece75c5b2d4686226f933859c4b0fa3cafae27
NoahK216 <115042520+NoahK216@users.noreply.github.com>
2024-09-11 21:43:30 -0400

    Committing this out of curiosity

--- src/routes/Home/Landing.tsx
+++ src/routes/Home/Landing.tsx
@@ -10,7 +10,7 @@ const Landing = ({ }) => {
                 <div>Hardware</div>
                 <div>Software</div>
                 <div>Sports</div>
-                <p>Digital Sports Solutions LLC.</p>
+                <p>Digital Sports Solutions LLC. {window.innerWidth}</p>
             </div>
             <div className="column image-paragraph">
                 <img src='https://computercare.net/wp-content/uploads/2019/08/Blog-Post-Header.png' />
