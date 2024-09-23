39e870a25ff017eaa3ff0c6170dfb71bd23220a4
NoahK216 <115042520+NoahK216@users.noreply.github.com>
2024-09-23 12:18:50 -0400

    Push for TS to not error deploy

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -5,7 +5,7 @@ import Footer from '../../common_components/Footer';
 import Landing from './Landing';
 import Partners from './Partners';
 import ProductCarousel from './ProductCarousel';
-import ContactButton from '../../common_components/ContactButton';
+//import ContactButton from '../../common_components/ContactButton';
 
 const Home = () => {
     return (
