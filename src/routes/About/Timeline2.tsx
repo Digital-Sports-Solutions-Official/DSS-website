596b607c65cf8293bb5a943d3f2ec4463f4c95d5
NoahK216 <115042520+NoahK216@users.noreply.github.com>
2024-09-23 23:35:25 -0400

    Fixed TS errors

--- src/routes/About/Timeline2.tsx
+++ src/routes/About/Timeline2.tsx
@@ -1,41 +1,6 @@
 /* Noah Klein */
-import React, { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './Timeline2.css';
-
-interface Event {
-    date: string;
-    description: string;
-}
-
-interface TimelineProps {
-    events: Event[];
-}
-
-
-const items = [
-    {
-        title: "Oct. 2022",
-        cardTitle: "The idea",
-        cardDetailedText: "Digital Sports Solutions began when Adam and Andrew were on the way home from playing in a National Collegiate Dodgeball Association (NCDA) tournament. They both recognized an opportunity to innovate on the NCDA’s manual shotclocking system by using their extensive engineering backgrounds to develop a more automated solution.",
-        media: {
-            type: "IMAGE",
-            source: {
-                url: "/timeline-pic.jpg"
-            }
-        }
-    },
-    {
-        title: "Oct. 2022",
-        cardTitle: "The NCDA",
-        cardDetailedText: "The NCDA is a nonprofit dodgeball league for college students specializing in the 8.5in pinch format. In dodgeball 2 teams go head-to-head with the goal of eliminating their opponent, similar to basketball there is a shotclock to keep the game moving. But unlike basketball each team has its own clock running asynchronously, each managed by a referee nicknamed shotclocker who is manually counting. Obviously, this creates issues because no two people count at the same pace meaning one side might be throwing more or less than the other. Furthermore, dodgeball is a high-intensity sport and a shotclocker might forget the count whenever the game gets chaotic. This is what inspired the creation of digital computer-controlled shotclocks to make the shotclockers job easier and less stressful.",
-        media: {
-            type: "IMAGE",
-            source: {
-                url: "/timeline-pic.jpg"
-            }
-        }
-    },
-];
 
 const events = [
     { date: '2022-01-01', description: 'Event 1 Description' },
@@ -45,11 +10,11 @@ const events = [
 ];
 
 const Timeline2 = () => {
-    const [animate, setAnimate] = useState(false);
+    // const [animate, setAnimate] = useState(false);
 
     useEffect(() => {
         // Trigger animation on mount
-        setAnimate(true);
+        // setAnimate(true);
     }, []);
 
 
