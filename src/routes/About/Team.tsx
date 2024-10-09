447b14e32089b21aa05e7e8d1eca01c04c68734d
NoahK216 <115042520+NoahK216@users.noreply.github.com>
2024-10-09 00:17:56 -0400

    Added title

--- src/routes/About/Team.tsx
+++ src/routes/About/Team.tsx
@@ -5,18 +5,21 @@ import './Team.css'
 const teamMembers = [
     {
         name: "Adam Flis",
+        title: "CTO",
         href: "https://www.linkedin.com/in/adam-flis/",
         description: "Founder and Chief Technical Officer of Digital Sports Solutions LLC. Adam is set to graduate from The Ohio State University in May 2025 with a B.S. in Computer Science and Engineering and a minor in Entrepreneurship and Innovation. Adam is the lead software developer for the Clkr hardware modules and drives company innovation with new ideas.",
         picture: "team/cute.jpg",
     },
     {
         name: "Andrew Flis",
+        title: "CEO",
         href: "https://www.linkedin.com/in/andrew-flis-12b665159/",
         description: "Co-Founder and Chief Executive Officer of Digital Sports Solutions LLC. Andrew is a graduate of The Ohio State University with a Bachelors in Electrical and Computer Engineering. Responsible for the hardware and electrical design of the Clkr hardware suite, along with the company's marketing and public relations.",
         picture: "team/andrew.jpg",
     },
     {
         name: "Michael Wilson",
+        title: "CIO",
         href: "https://www.linkedin.com/in/michael-wilson-0bb38b208/",
         description: "Chief Information Officer of Digital Sports Solutions LLC. Michael is a Ohio State University alumni, who graduated in Fall of 2023. A previous Artificial Intelligence Engineer, Michael leads the technical design process team at DSS",
         picture: "team/mike.JPG",
@@ -37,7 +40,8 @@ const Team = () => {
                             <a href={member.href} target='_blank'>
                                 <img src={member.picture} alt="Foreground" className="foreground-image" />
                             </a>
-                            <h3>{member.name}</h3>
+                            <h3 className='name'>{member.name}</h3>
+                            <h3 className='title'>{member.title}</h3>
 
 
                             <img src="logos/DSS_LogoMark_FullColor.svg" alt="Background" className="background-image" />
